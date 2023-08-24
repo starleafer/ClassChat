fece45abc9598732caeb43eac991d665c191d37d
Emil Stjernlöf <emil.stjernlof@gmail.com>
2023-08-24 13:09:45 +0200

    test

--- class-chat/components/screens/Login.js
+++ class-chat/components/screens/Login.js
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet } from 'react-native'
 export default function Login() {
   return (
     <View style={styles.container}>
-        <Text>HEJ</Text>
+        <Text>HEEEEJ</Text>
     </View>
   )
 }
