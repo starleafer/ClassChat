a02538d238119c5691e53271b9ac1c8a6ffb67cf
Emil Stjernlöf <emil.stjernlof@gmail.com>
2023-08-24 13:05:06 +0200

    Init

---
+++ class-chat/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Login from './components/screens/Login';
+
+export default function App() {
+  return (
+    <View >
+      <Login />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: '#fff',
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  // },
+});
---
+++ class-chat/components/screens/Login.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+export default function Login() {
+  return (
+    <View style={styles.container}>
+        <Text>HEJ</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'tomato'
+    }
+})
