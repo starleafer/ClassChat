6f3f6315014e3cc72ed3c08bf8ec9b8d815ab0a1
tobionesies <d2ba5@pm.me>
2023-08-30 11:23:40 +0200

    added message delete function, reorder of flatlist

--- class-chat/components/screens/Chat.js
+++ class-chat/components/screens/Chat.js
@@ -1,7 +1,7 @@
 import { Button, StyleSheet, Text, View } from "react-native";
 import React, { useContext, useState, useEffect } from "react";
 import { AuthContext } from "../contexts/AuthContext";
-import { FlatList, TextInput } from "react-native-gesture-handler";
+import { FlatList, TextInput, TouchableOpacity } from "react-native-gesture-handler";
 
 const Chat = () => {
   const { accessToken, isLoggedIn, username } = useContext(AuthContext);
@@ -29,93 +29,93 @@ const Chat = () => {
       console.log("fetchAllMessages catch ->" + error);
     }
   };
+  const deleteMessage = async (messageId) => {
+    // console.log(username)
+    try {
+      const response = await fetch(
+        "https://chat-api-with-auth.up.railway.app/messages/"+ messageId,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + accessToken,
+          },
+        }
+      );
+
+      const msgLog = await response.json();
+      fetchAllMessages();
+    } catch (error) {
+      console.log("fetchAllMessages catch ->" + error);
+    }
+  };
 
   const createMessage = async () => {
     try {
-      await fetch("https://chat-api-with-auth.up.railway.app/messages",
-      {
+      await fetch("https://chat-api-with-auth.up.railway.app/messages", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
           Authorization: "Bearer " + accessToken,
         },
-        body: JSON.stringify(
-            {
-                content: textMsg
-            })
-      }
-    );
+        body: JSON.stringify({
+          content: textMsg,
+        }),
+      });
 
-    setTextMsg('')
-    fetchAllMessages()
-      
+      setTextMsg("");
+      fetchAllMessages();
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
     // isLoggedIn();
     fetchAllMessages();
   }, []);
 
-  // console.log(messages.data[1].user.username)
-  // superHeroes["members"][1]["powers"][2];
-
-
-  // const rottenTomatoesRating = details.Ratings.find(rating => rating.Source === 'Rotten Tomatoes');
-  // if (rottenTomatoesRating) {
-  //   rottenTomatoesValue = rottenTomatoesRating.Value;
-  // }
-
-  // const findUsername = messages.data.user.find(name => name === 'username');
-  //   if (findUsername) {
-  //     foundName = findUsername.value;
-  //   }
-
-  // console.log(messages.data[2].user.username)
-
-  // const usernames = messages.data.map(names => names.user.username)
-  // console.log(usernames)
-
-
   return (
     <View style={styles.container}>
-      {/* {messages.status == 200 
-        ? messages.data.map((message) => {
-          <View key={message._id} style={styles.textContainer}>
-            <Text>{message.content}</Text>
-          </View>
-        }) 
-       : null} */}
-
-      {/* <FlatList
+      <FlatList
         inverted
-        data={messages.data}
+        data={messages.data.reverse()}
+        keyExtractor={(item, index) => index}
         renderItem={({ item }) => (
           <View style={styles.textContainer}>
-           { messages.data !== undefined
-              ?  "testing53" == "testing53" 
-                  ? <Text style={styles.userMessage}>{item.content}</Text>
-                  : <Text style={styles.message}>{item.date}{item.content}</Text>             
-              : <Text>Loading</Text>
-            }
-            <Text style={styles.userMessage}>{item.date}{item.content}</Text>
+            {messages.data !== null ? (
+              item.user !== null ? (
+                item.user.username === username ? (
+                  <TouchableOpacity onLongPress={() => {deleteMessage(item._id)}} delayLongPress={2000}>
+                    <Text style={styles.userMessage}>
+                      {item.content}{item.user.username}
+                    </Text>
+                  </TouchableOpacity>
+                ) : (
+                  <Text style={styles.message}>
+                    {item.user.username}
+                    {item.date}
+                    {item.content}
+                  </Text>
+                )
+              ) : (
+                <Text style={styles.message}>
+                  {item.date}
+                  {item.content}
+                </Text>
+              )
+            ) : (
+              <Text>Loading..</Text>
+            )}
           </View>
         )}
-        keyExtractor={item => item._id} /> */}
-        <FlatList 
-          inverted
-          data={Object.entries(messages.data)}
-          keyExtractor={(item, index) => index}
-          renderItem={({item}) => {
-          }}
-        />
-        
-
-      <TextInput style={styles.msgInput} value={textMsg} onChangeText={(msg) =>setTextMsg(msg)}></TextInput>
-      <Button title="Skicka" onPress={() => createMessage()}/>
-
+      />
+      <TextInput
+        style={styles.msgInput}
+        value={textMsg}
+        onChangeText={(msg) => setTextMsg(msg)}
+      ></TextInput>
+      <Button title="Skicka" onPress={() => createMessage()} />
     </View>
   );
 };
@@ -126,25 +126,34 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center",
+    // alignItems: "center",
     backgroundColor: "skyblue",
   },
   textContainer: {
     marginVertical: 10,
-    borderWidth: 1,
+    // borderWidth: 1,
+    // paddingHorizontal: 5,
+    // width: '70%'
   },
   message: {
-    flexDirection: 'column',
-    color: 'green',
+    flexDirection: "column",
+    color: "green",
+    width: 250,
+    paddingHorizontal: 5,
+    marginHorizontal: 3,
+    borderWidth: 1,
   },
   msgInput: {
     borderWidth: 1,
-    backgroundColor: '#fff',
-    width: '100%',
+    backgroundColor: "#fff",
+    width: "100%",
     marginVertical: 10,
     padding: 5,
   },
   userMessage: {
-    color: 'red',
-  }
+    color: "red",
+    textAlign: "right",
+    paddingHorizontal: 5,
+    marginHorizontal: 3,
+  },
 });
